7c191b29292bb5c9d33d6b0bdb505ea1155d893f
Pablo Henriquez <p.henriquezd@outlook.com>
2021-02-11 12:34:13 -0300

    loading page con diseño

--- src/components/shared/loading.js
+++ src/components/shared/loading.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
 
 const LoadingComponent = () => (
-    <h1>Loading...</h1>
+    <div className="loading-component">
+        <Typography variant="h2">
+            Tiendas
+        </Typography>
+        <CircularProgress color="primary"/>
+    </div>
 );
 
 export default LoadingComponent;
