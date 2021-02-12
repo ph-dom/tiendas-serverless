6102f7dabf5a5821e168e54b2ca7811b0172118e
Pablo Henriquez <p.henriquezd@outlook.com>
2021-02-12 15:45:12 -0300

    configuracion de firebase con variablez de ambiente

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,24 +3,15 @@ import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseConfig = process.env.NODE_ENV === 'production' ? {
-    apiKey: "AIzaSyDOWenZ-ApGVyLMrMriRl6DguvG5udlhyU",
-    authDomain: "tiendas-app20.firebaseapp.com",
-    databaseURL: "https://tiendas-app20.firebaseio.com",
-    projectId: "tiendas-app20",
-    storageBucket: "tiendas-app20.appspot.com",
-    messagingSenderId: "1046589679862",
-    appId: "1:1046589679862:web:b07379bd6f822963a124d7",
-    measurementId: "G-8HZH2KL73Z"
-} : {
-    apiKey: "AIzaSyBHgyIcKKhla6OVIyux6_Cn5XxLW6t-e3E",
-    authDomain: "tiendas-app20dev.firebaseapp.com",
-    databaseURL: "https://tiendas-app20dev.firebaseio.com",
-    projectId: "tiendas-app20dev",
-    storageBucket: "tiendas-app20dev.appspot.com",
-    messagingSenderId: "698020383385",
-    appId: "1:698020383385:web:8eaa94822357209cb98608",
-    measurementId: "G-YPVENQJE8B"
+const firebaseConfig = {
+    apiKey:             process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain:         process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL:        process.env.REACT_APP_FIREBASE_DATABASEURL,
+    projectId:          process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket:      process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId:              process.env.REACT_APP_FIREBASE_APPID,
+    measurementId:      process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 const app = firebase.initializeApp(firebaseConfig);
